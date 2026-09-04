4b8d5ea67f02c7f266da2c1f6f134bec73658853
cnmi-blood <parichat.inkliang@gmail.com>
2026-09-04 18:13:29 +0700

    Add files via upload
    Blood_QC_WebApp_v5_3_16_UPDATE_ONLY

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'blood-qc-v5.3.15';
+const VERSION = 'blood-qc-v5.3.16';
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
